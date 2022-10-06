8aa78549033148c3f19ca165f1f981ef53176a20
Максим Крыжновский <maxkryzh@MacBook-Air-Maksim.local>
2022-10-06 18:42:35 +0300

    header fixed

--- src/index.js
+++ src/index.js
@@ -11,9 +11,10 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </PersistGate>
   </Provider>
 );
+// basename = '/goit-react-hw-08-phonebook/';
